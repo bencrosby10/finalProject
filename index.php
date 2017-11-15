4a89ccfdef70c70d75c831367dc90730e7f6bbea
bencrosby10 <33334560+bencrosby10@users.noreply.github.com>
2017-11-15 12:28:03 -0500

    Update index.php

--- index.php
+++ index.php
@@ -3,8 +3,9 @@
 ?>
 
 <article class="index">
-    <h2>Welcome to MHYC..........</h2>
-    <p>Add more and stuff</p>
+    <h2>Welcome to MHYC</h2>
+     <img src="images/club.jpg" alt="club-house">
+    <p>The Maple Hill Yacht Club was founded in 1935 based on the principles of sailing and friendship which are the cornerstones on which the club operates today. MHYC is a private club and is 100% owned and operated by its members. MHYC sponsors many boating and social events throughout the summer season. The club also hosts and operates a junior sailing program that runs for eight weeks during the summer season. The Junior program is taught by US Sailing level One certified instructors and is a highlight of the club for members and their children alike.</p>
 
 
 
