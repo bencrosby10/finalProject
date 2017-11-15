237c9e52e29d5155b49b47e9d6bff0e1914f8b05
bencrosby10 <33334560+bencrosby10@users.noreply.github.com>
2017-11-15 12:14:12 -0500

    event page update

--- events.php
+++ events.php
@@ -14,7 +14,7 @@
             </div>
             <div class="events-card-text">
                 <h2 class="events-card-title">Spring Clean-up</h2>
-                <h4 class="events-card-subtitle">Saturday April 8<sup>th</sup><br>9:00 am - 12:00pm</h4>
+                <h4 class="events-card-subtitle">Saturday April 8<sup>th</sup> 2018<br>9:00am - 12:00pm</h4>
                 <p class="events-card-tags">As spring is upon us it’s time to get boats ready and clan up around the club house. We are looking for eager hands to help clean up downed sticks from winter as well as spreading crushed rock around the junior sailing boat ramp. There will be coffee and donuts provided. Come out and help get the facility looking good for the coming season!</p>
             </div>
 
@@ -26,7 +26,7 @@
             </div>
             <div class="events-card-text">
                 <h2 class="events-card-title">18<sup>th</sup>Annual ACS Regatta</h2>
-                <h4 class="events-card-subtitle">Saturday July 15<sup>th</sup><br>9:00 am - 12:00pm</h4>
+                <h4 class="events-card-subtitle">Saturday July 14<sup>th</sup> 2018<br>9:00am - 6:00pm</h4>
                 <p class="events-card-tags">Fellow New England Sailors,
                     The MHYC is shooting for 40 boats at this year’s event. All sailors have the opportunity to get directly involved & raise money to cure cancer. This event will start with an island race in the morning and be followed by a live auction and cook out at the club. Please distribute this to all your sailing friends to inform and access all potential competitors. The more fundraisers, the more money and awareness for The American Cancer Society!
                 </p>
@@ -43,7 +43,7 @@
                     <a href="dinner-form.php">
                         <h2 class="events-card-title">Annual Lobster Dinner!</h2>
                     </a>
-                    <h4 class="events-card-subtitle">Saturday August 19<sup>th</sup><br>5:00 PM</h4>
+                    <h4 class="events-card-subtitle">Saturday August 18<sup>th</sup> 2018<br>5:00pm</h4>
                     <p class="events-card-tags">At only $25, it's a great way of showing support for Maple Hill Junior Sailing, while having an amazing meal and a fun evening. Bring family and friends (children welcome, of course !) Meet old friends, make new ones. See how far MHJS has come, appreciate the work of staff and volunteers, and enjoy! Get your tickets by creating a reservation <a href="dinner-form.php">HERE</a> The event will be held at the Maple Hill Yacht Club (As always, non-seafood choices offered and BYOB.)
                     </p>
                 </div>
@@ -55,8 +55,8 @@
                 <img class="events-card-img" src="images/fall-meeting.png" alt="Annual-Meeting ">
             </div>
             <div class="events-card-text">
-                <h2 class="events-card-title">LWSA Annual Meeting Dinner</h2>
-                <h4 class="events-card-subtitle">October 12<sup>th</sup></h4>
+                <h2 class="events-card-title">MHYC Annual Meeting Dinner</h2>
+                <h4 class="events-card-subtitle">Friday October 12<sup>th</sup> 2018<br>6:00pm</h4>
                 <p class="events-card-tags">All members are encouraged to attend the Annual Meeting dinner at the Lyon's Den. It is a fun evening, good food and friends, and a wonderful recap of the sailing season for the J/80 Fleet and PHRF Racing / Mixed Fleet, and the Sailing School. It is also a chance to have a say in our leadership and governance of the organization. You can register by emailing <a href="mailto:events@mhyc.com">events@mhyc.com</a>
                 </p>
             </div>
@@ -70,7 +70,7 @@
             </div>
             <div class="events-card-text">
                 <h2 class="events-card-title">Fall Project Day at the club</h2>
-                <h4 class="events-card-subtitle">Saturday October 14<sup>th</sup><br>8:00 AM - 12:00 PM</h4>
+                <h4 class="events-card-subtitle">Saturday October 13<sup>th</sup>2018<br>8:00am - 12:00pm</h4>
                 <p class="events-card-tags">As we close up the yacht club for the winter, there are a few projects we will be trying to complete on the buildings, boats, and grounds. Whether you are handy with a skill or just happy to come lend a hand, please join us for the morning to get things ship-shape. Also, prior to the project day, there is a project to paint the trim on the club, spare pairs of hands needed,
                     if you have a few hours to spare any sunny day. Please contact <a href="mailto:Jeff.Rabinowitz@worms.com">Jeff.Rabinowitz@worms.com</a> if you have any questions.
                 </p>
@@ -80,11 +80,11 @@
 
         <div class="events-card">
             <div class="events-card-img">
-                <img class="events-card-img" src="images/ACS-regatta.png" alt="ACS-Regatta">
+                <img class="events-card-img" src="images/youth4.jpg" alt="youth-regatta">
             </div>
             <div class="events-card-text">
                 <h2 class="events-card-title">Youth Regatta (W.A.R.)</h2>
-                <h4 class="events-card-subtitle">8:00am- 3:00pm Saturday July 19<sup>th</sup><br>9:00 am - 12:00pm</h4>
+                <h4 class="events-card-subtitle">Saturday July 19<sup>th</sup> 2018<br>8:00am- 3:00pm</h4>
                 <p class="events-card-tags">The Wicked Awesome Regatta (W.A.R.) is organized and Hosted by the Maple Hill Yacht club Junior Sailing program every July. Youth sailors and youth sailing organizations from across New England, New York, and adjacent regions always ensure that the W.A.R. is a welcoming, spirited competition with a fun time had by all. The Maple Hill yacht Club has generously provided the venue for the event for a number of years.  The next W.A.R. is scheduled for July 19, 2018.  For more information, email us at the juniorsailing@mhyc.com
                     
                 </p>
